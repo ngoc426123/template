9c8dd556bc13643087e70e98defe9980b4050560
MinhNgoc <minhngoc.ith@gmail.com>
2021-10-09 21:36:25 +0700

    21:35 09/10/2021: Update devDepencies, update gulp build style.

--- gulpsrc/task/build-style.js
+++ gulpsrc/task/build-style.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const bulkSass = require('gulp-sass-bulk-import');
 const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
