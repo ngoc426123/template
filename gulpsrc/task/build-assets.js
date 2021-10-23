e67db0d27932ac3fc06660046fe13ccf92457f46
MinhNgoc <minhngoc.ith@gmail.com>
2021-10-23 21:40:14 +0700

    21:40 23/10/2021: Add file customs of js/css

--- gulpsrc/task/build-assets.js
+++ gulpsrc/task/build-assets.js
@@ -1,13 +1,17 @@
 const { series } = require('gulp');
 const styles = require('./build-style');
+const stylesCustoms = require('./build-style-customs');
 const scripts = require('./build-script');
+const scriptsCustoms = require('./build-script-customs');
 const data = require('./build-data');
 const fonts = require('./build-fonts');
 const images = require('./build-images');
 
 const buildAssets = series(
   styles,
+  stylesCustoms,
   scripts,
+  scriptsCustoms,
   data,
   fonts,
   images
--- src/js/_init/import-jquery-plugin.js
+++ src/js/_init/import-jquery-plugin.js
@@ -1,5 +1,4 @@
 import 'slick-carousel';
-import jQuery from 'jquery'
 
 window.jQuery = jQuery;
 window.$ = jQuery;
