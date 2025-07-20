2c6343a1c2054b8d9b4fa118ade3d8a1a7010149
Minh Ngoc <ngoc.hm@sutrixsolution.com>
2025-07-20 14:06:14 +0700

    Updates

---
+++ js/customs.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+  /*  
+  *   CUSTOM SCRIPT IN HERE, YOU CAN WRITE EVERY THINGS WITHOUT FEAR AFFECT TO MAIN SCRIPT
+  *   ALLWAY PUT THIS FILE BELOW app.min.js
+  */
+  
+})
